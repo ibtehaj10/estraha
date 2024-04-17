cc37cf4a4d95237625da7201c629102d4e70746d
Hafiz Ibtehaj Samir Khan <51523656+ibtehaj10@users.noreply.github.com>
2024-04-17 13:24:59 +0500

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -214,8 +214,8 @@ def check_user():
                 send = gpt(chats)
                 reply = send.choices[0].message.content
                 write_chat({"role":"assistant","content":reply},path)   
-                return Response(reply, mimetype='text/html')
-                # return {"message":reply,"status":"OK"}
+                # return Response(reply, mimetype='text/html')
+                return {"message":reply,"status":"OK"}
             else:
                 print("we hare at 2")
                 write_chat({"role":"user","content":prompt+"""make sure  to return it in '`{"city":"مكه"}`' formate """},path)
@@ -228,15 +228,15 @@ def check_user():
                 jsons = str_to_json(str(get[0]))
                 print("We got JSON : ",jsons)
                 listing = findproperty_citywise(jsons['city'])
-                return Response(reply, mimetype='text/html')
-                # return {"message":reply,"status":"OK"}
+                # return Response(reply, mimetype='text/html')
+                return {"message":reply,"status":"OK"}
 
 
         else:
             print("reply    ",reply)
             write_chat({"role":"assistant","content":reply},path)
-            return Response(reply, mimetype='text/html')
-            # return {"message":reply,"status":"OK","images":[]}
+            # return Response(reply, mimetype='text/html')
+            return {"message":reply,"status":"OK","images":[]}
         # except:
         #     return {"message":"something went wrong!","status":"404"}
 
