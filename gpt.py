951f3ff834d5b9da4cef44083c5cc7cb34a42461
Hafiz Ibtehaj Samir Khan <51523656+ibtehaj10@users.noreply.github.com>
2024-06-09 16:40:29 +0500

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -76,7 +76,10 @@ def findproperty_citywise(city):
     url = 'https://www.estraha.com/property-detail/'
     for record in js:
         record['URL'] = url + str(record['property ID'])
-    return str(js[:6])
+    if property.empty:
+        return "No Property found in this city"
+    else:
+        return str(js[:6])
     
     
     
